5da59d603251798e7dde106823cf6475cce5541e
keke <zhengkefeng15@gmail.com>
2014-01-10 18:17:39 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+
+/*
+ * GET home page.
+ */
+
+var util = require('util')
+  , formidable = require('formidable')
+  , fs = require('fs')
+  , imagemagick = require('imagemagick')
+  , sizeOf = require('image-size');
+
+exports.index = function(req, res){
+  res.render('index', {
+    title: '专题生成器'
+  });
+};
+
+exports.up = function(req, res) {
+  var form = new formidable.IncomingForm();
+
+  form.uploadDir = './public/images/uploads/' || process.cwd();
+  form.keepExtensions = true;
+
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      console.log(err);
+      res.end('出错了');
+      return;
+    }
+    // fs.rename(files.pic.path, './public/uploads/' + files.pic.name);
+    imagemagick.convert([files.pic.path, '-crop', '1920x150', 'public/images/uploads/' + files.pic.name], function(err, stdout) {
+        if (err) throw err;
+
+        console.log(util.inspect(stdout));
+        sizeOf(files.pic.path, function(err, metadata) {
+          if (err) throw err;
+
+          var smpic = {
+            width: metadata.width,
+            height: metadata.height,
+            pics: []
+          }
+          
+          for (var i = 0; i <= parseInt(metadata.height / 150); i++) {
+            smpic.pics[i] = '/images/uploads/' + files.pic.name.replace('.jpg', '-' + i + '.jpg');
+          }
+
+          res.render('index', {
+              pic: files.pic.path.split('public')[1],
+              smpic: smpic
+          })
+
+        })
+
+    })
+  })
+
+  form.on('end', function(err, fields, files) {
+  });
+};
